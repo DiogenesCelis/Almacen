166dab22da15c537edf1b9bf38420ced06657bb8
@DiogenesCelis <USUARIO@IdeaPad-Celis>
2022-11-17 23:40:07 -0500

    Añadí todas las opciones de menú, las opciones completas son 1-4 y 6,12. Falta solucionar la opcion 5 y de allí desplegar las demás.

--- src/Consesionario/Almacen.java
+++ src/Consesionario/Almacen.java
@@ -14,14 +14,13 @@ public class Almacen {
     /**
      * @param args the command line arguments
      */
-    public static Auto ajjas[];
-
     public static void main(String[] args) {
         Scanner x = new Scanner(System.in);
         Random r = new Random();
         int n = 0, n2 = 0, sw = 0, op, mn;
-        Auto inventario[];
-        vendedor empleados[];
+        Auto inventario[] = new Auto[1];
+        vendedor empleados[] = new vendedor[1];
+        ventas registroVenta[] = new ventas[1];
 
         //<editor-fold defaultstate="collapsed" desc="codigo">
         do {
@@ -32,18 +31,21 @@ public class Almacen {
                         JOptionPane.PLAIN_MESSAGE, null, new Object[]{"Selecciona una opción",
                             "1. Registrar autos", "2. Registrar autos automaticamente",
                             "3. Registrar vendedores", "4. Registrar vendedores automaticamente",
-                            "5. Insertar venta y liquidar comisión", "6. Salir"},
+                            "5. Insertar venta y liquidar comisión", "6. Aplicar descuento",
+                            "7. Aplicar descuento a todos", "8. Cantidad de autos disponibles",
+                            "9. Cantidad de autos vendidos", "10. Cantidad y total de comision por vendedor", "11. Total venta y comisiones", "12. Salir"},
                         "Selecciona")).toString();
-                menu = menu.substring(0, 1);
+                menu = menu.substring(0, 2);
+                System.out.println(menu);
 
                 switch (menu) {
 
-                    case "S":
+                    case "Se":
                         sww = 1;
 
                         break;
-                        
-                    case "1":
+
+                    case "1.":
                         //<editor-fold defaultstate="collapsed" desc="Llenado vehiculo">
                         System.out.println("\033[35m**Llenado de vehiculo**");
                         n = Integer.parseInt(JOptionPane.showInputDialog("Ingrese cantidad de autos a registrar"));
@@ -105,7 +107,7 @@ public class Almacen {
                         sww = 1;
                         break;
 
-                    case "2":
+                    case "2.":
                         //<editor-fold defaultstate="collapsed" desc="LLenado automatico vehiculo">
                         System.out.println("\033[35m**Llenado de vehiculo automatico**");
                         n = Integer.parseInt(JOptionPane.showInputDialog("Ingrese cantidad de autos a registrar"));
@@ -147,6 +149,7 @@ public class Almacen {
                             System.out.println("Kilometraje: " + inventario[j].getKilometraje() + "km");
                             System.out.println("Modelo: " + inventario[j].getModelo());
                             System.out.println("Precio: $" + inventario[j].getPrecio() + " Dlls");
+                            System.out.println("Estado: " + inventario[j].getEstado());
                             System.out.println("\n");
                         }
 //</editor-fold>
@@ -154,7 +157,7 @@ public class Almacen {
 ////                Auto.llenadoAutom();
                         break;
 
-                    case "3":
+                    case "3.":
                         //<editor-fold defaultstate="collapsed" desc="Llenado Empleados">
                         System.out.println("\033[35m**Llenado de empleados**");
                         n2 = Integer.parseInt(JOptionPane.showInputDialog("Ingrese cantidad de vendedores a registrar"));
@@ -182,7 +185,7 @@ public class Almacen {
                         //</editor-fold>
                         sww = 1;
                         break;
-                    case "4":
+                    case "4.":
                         //<editor-fold defaultstate="collapsed" desc="Llenado empleados automatico">
                         System.out.println("\033[35m**Llenado de empleados automatico**");
                         n2 = Integer.parseInt(JOptionPane.showInputDialog("Ingrese cantidad de empleados a registrar"));
@@ -196,15 +199,14 @@ public class Almacen {
                         String inicial[] = {"A", "B", "C", "D", "E", "F", "G", "H", "I", "J", "K", "L", "M", "N", "O", "P", "Q", "R", "S", "T", "U", "V", "W", "X", "Y", "Z"};
 
                         for (int i = 0; i < n2; i++) {
-                            op = r.nextInt(1, 5);
-                            vendedor pj = new vendedor("", i, 0);
+                            vendedor pj = new vendedor("", 1, 0);
 
                             pj.setNombre(nombres[r.nextInt(1 * 18)] + " " + inicial[r.nextInt(1 * 24)] + ". " + apellidos[r.nextInt(1 * 11)]);
                             pj.setIdentificacion(r.nextInt(1140899101));
                             empleados[i] = pj;
 
                         }
-                        for (int j = 0; j < n; j++) {
+                        for (int j = 0; j < n2; j++) {
                             JOptionPane.showMessageDialog(null, "**Empleado " + (j + 1) + "**", "Vendedores Registrados Automaticamente", 1);
                             JOptionPane.showMessageDialog(null, "Nombre: " + empleados[j].getNombre()
                                     + "\nCedula: " + empleados[j].getIdentificacion(),
@@ -219,31 +221,104 @@ public class Almacen {
 //</editor-fold>
                         sww = 1;
                         break;
-                    case "5":
+                    case "5.":
 
                         //<editor-fold defaultstate="collapsed" desc="Registrar Venta">
                         System.out.println("\033[35m**Registrar venta y comisión**");
 
                         System.out.println("Ingrese Placa del vehiculo");
-                        String BsqPlaca = x.nextLine();
+                        String BsqPlaca = JOptionPane.showInputDialog("Ingrese placa del vehiculo");
                         System.out.println(BsqPlaca);
+                        ventas venta = new ventas();
+                        int posAuto = 0,
+                         placaDisp = 0,
+                         placaExis = 0;
 
-                        System.out.println("Ingrese cedula del vendedor");
-                        int BsqCC = x.nextInt();
-                        System.out.println(BsqCC);
+                        for (int i = 0; i < n; i++) {
 
-                        for (int i = 0; i < n2; i++) {
-//                            System.out.println(inventario[i].getPlaca());
+                            if (BsqPlaca == inventario[i].getPlaca()) {
+//                                System.out.println("El vehiculo está disponible");
+
+                                System.out.println("\033[32m**Vehiculo " + (i + 1) + "**");
+                                System.out.println("Marca: " + inventario[i].getMarca());
+                                System.out.println("Placa: " + inventario[i].getPlaca());
+                                System.out.println("Color: " + inventario[i].getColor());
+                                System.out.println("Kilometraje: " + inventario[i].getKilometraje() + "km");
+                                System.out.println("Modelo: " + inventario[i].getModelo());
+                                System.out.println("Precio: $" + inventario[i].getPrecio() + " Dlls");
+                                System.out.println("\n");
+
+                                posAuto = i;
+                                placaExis = 1;
+                            }
 
                         }
+
+//                        if(placaExis==1){
+                        if (inventario[posAuto].getEstado() == 'D') {
+                            placaDisp = 1;
+                        } else {
+                            placaDisp = 0;
+                            System.out.println("La placa no está disponible");
+                        }
+                        System.out.println(posAuto);
+                        System.out.println(registroVenta[1]);
+//                        System.out.println("Ingrese cedula del vendedor");
+//                        int BsqCC = x.nextInt();
+//                        System.out.println(BsqCC);
+
+                        //<editor-fold defaultstate="collapsed" desc="registro venta">
+//                        venta.setPlaca(BsqPlaca);
+//                        venta.setCedula(posAuto);
+//                        venta.setComision(posAuto);
+//                        registroVenta[1]=venta;
+//</editor-fold>
 //</editor-fold>
                         sww = 1;
 
                         break;
 
-                    case "6":
+                    case "6.":
+                        
+                        sww = 1;
+                        break;
+
+                    case "7.":
+
+                        sww = 1;
+                        break;
+
+                    case "8.":
+
+                        //<editor-fold defaultstate="collapsed" desc="Contar unidades disponibles">
+                        int unidadesDisp = 0;
+                        for (int i = 0; i < n; i++) {
+
+                            if (inventario[i].getEstado() == 'D') {
+                                unidadesDisp = unidadesDisp + 1;
+                            }
+                        }
+                        JOptionPane.showMessageDialog(null, "Existen " + unidadesDisp + " unidades disponibles", "Unidades en inventario", JOptionPane.INFORMATION_MESSAGE);
+//</editor-fold>
+
+                        sww = 1;
+                        break;
+
+                    case "9.":
+
+                        sww = 1;
+                        break;
+
+                    case "10":
+                        sww = 1;
+                        break;
+
+                    case "12":
+
+                        JOptionPane.showMessageDialog(null, "      Muchas gracias por su tiempo", "Salida", -1);
                         System.out.println("\033[35m**Salir**");
                         System.out.println("Muchas gracias por su tiempo");
+
                         sww = 1;
                         sw = 1;
 
@@ -260,5 +335,4 @@ public class Almacen {
 //</editor-fold>
 
     }
-//    2da carga
 }
